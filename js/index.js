b93662f107782cf7a6138ae0cfb85803fa372437
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-02-24 00:12:38 +0100

    Added user options on main page

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 import { baseUrl } from "./settings/api.js";
 import { displayMessage, createMenu } from "./components/index.js";
+import { getUsername } from "./utils/storage.js";
 
 createMenu();
 
 const createProducts = async () => {
   const productContainer = document.querySelector(".product-container");
+  const username = getUsername();
 
   try {
     const productsData = await (await fetch(`${baseUrl}products`)).json();
 
     productContainer.innerHTML = "";
     productsData.map((product) => {
+      let editPage = "";
+      if (username) {
+        editPage = `<a href="edit.html?id=${product.id}">Edit</a>`;
+      }
+
       productContainer.innerHTML += `
         <div class="product">
             <h4>${product.name}</h4>
             <p>Price: ${product.price}</p>
             <a href="detail.html?id=${product.id}">Details</a>
-            <a href="edit.html?id=${product.id}">Edit</a>
+            ${editPage}
         </div>`;
     });
   } catch (error) {
